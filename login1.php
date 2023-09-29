00284f997da6d42d58ca34736f1f3fde00522478
Taufiq233 <145994298+Taufiq233@users.noreply.github.com>
2023-09-29 22:28:24 +0700

    Add files via upload

---
+++ login1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+	<div class="kotak_login">
+        <img src="Vector.png" alt="vector">
+        <h3 class="tulisan_login">Log in to Tonality</h3>
+ 
+		<form>
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" >
+			<label>Password</label>
+			<input type="text" name="password" class="form_login" >
+            <i class="fa-regular fa-eye" id="eye"></i>
+            <i class="fa-regular fa-eye-slash" id="eye-slash"></i>
+            <center><input type="submit" class="tombol_login" value="LOGIN"></center>
+
+			<br/>
+			<br/>
+			<center>
+				<label>Don't have account? <a href="#" class="link"> Sign up for Tonality</a></label>
+			</center>
+		</form>
+		
+	</div>
+ 
+ 
+</body>
+</html>
